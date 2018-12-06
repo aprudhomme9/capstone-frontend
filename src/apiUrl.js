06e0216bb55fa2e5417658761a13664d69482350
Andy Prudhomme <aprudhomme9@gmail.com>
2018-12-05 19:49:09 -0600

    typo in production/development discernment code

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -2,7 +2,7 @@ console.log(Object.keys(process.env));
 
 let serverUrl;
 
-if(Object.keys(process.env).findIndex((key)=>key=='REACT_APP_DEPLOYED_VERSION'===-1)){
+if(Object.keys(process.env).findIndex((key)=>key=='REACT_APP_DEPLOYED_VERSION') === -1){
 	serverUrl = 'https://watch-with-friends-express.herokuapp.com/'
 } else {
 	serverUrl = 'http://localhost:5000/'
